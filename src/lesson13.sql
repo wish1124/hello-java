0343f470ab9551390581c8a479da48d2c479c818
wish1124 <ehddnrfndi@gmail.com>
2024-09-18 13:59:59 +0900

    수정

--- src/lesson13.sql
+++ src/lesson13.sql
@@ -1,15 +1,30 @@
-/*SELECT department_name
+SELECT dept_name, COUNT(emp_no)
 FROM departments
-GROUP BY department_name
-HAVING COUNT(employee_id) <= 22000;
+         INNER JOIN dept_emp
+                    ON departments.dept_no = dept_emp.dept_no
+GROUP BY departments.dept_no
+HAVING COUNT(emp_no) <= 22000;
 
-SELECT job_title, MAX(salary) AS highest_salary
+# employees <-> titles
+# employees <-> salaries
+
+SELECT titles.title, MAX(salary) AS highest_salary
 FROM employees
-GROUP BY job_title
+         JOIN titles
+              ON employees.emp_no = titles.emp_no
+         JOIN salaries
+              ON employees.emp_no = salaries.emp_no
+GROUP BY title
 HAVING MAX(salary) IS NOT NULL;
 
-SELECT department_name
+# departments <-> dept_emp <-> salaries
+# 급여 평균이 60,000 이상인 부서들을 출력하라.
+SELECT departments.dept_name
 FROM departments
-GROUP BY department_name
+JOIN dept_emp
+ON departments.dept_no = dept_emp.dept_no
+JOIN salaries
+ON dept_emp.emp_no = salaries.emp_no
+GROUP BY dept_name
 HAVING AVG(salary) >= 60000;
- */
+ # 부서명,부서 ,직원, 부서id 직원.부서id 그룹바이 부서명 평균 직원급여
